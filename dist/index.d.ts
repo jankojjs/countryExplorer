8eb3b0fc898295d053b5e3071b01de594c71cd06
Janko Stanic <janko.stanic@activecollab.com>
2024-08-06 08:52:11 +0200

    Add getCountryByCallingAndAreaCode and getCountry

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,7 @@
 import { countriesArraySortedByName, Country, CountryCode, countryCodeToCountryDataMap, CountryMap, CallingCodeToCountryCodeMap, callingCodeToCountryCodeMap } from "helpers";
+export declare const getCountry: (code: CountryCode) => Country;
 export declare const getCountryCallingCode: (code: CountryCode) => string;
+export declare const getCountryByCallingAndAreaCode: (callingCode: string) => CountryCode;
 export declare const getCountryFlag: (code: CountryCode) => string;
 export declare const getCountries: () => Country[];
 export { countryCodeToCountryDataMap, countriesArraySortedByName, callingCodeToCountryCodeMap };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 // @/src/index.ts
 
-import { countriesArraySortedByName, Country, CountryCode, countryCodeToCountryDataMap, CountryMap, CallingCodeToCountryCodeMap, callingCodeToCountryCodeMap } from "helpers";
+import { 
+    countriesArraySortedByName,
+    Country, 
+    CountryCode, 
+    countryCodeToCountryDataMap, 
+    CountryMap, 
+    CallingCodeToCountryCodeMap, 
+    callingCodeToCountryCodeMap 
+} from "helpers";
+
+export const getCountry = (code: CountryCode): Country => {
+    return countryCodeToCountryDataMap[code];
+}
 
 export const getCountryCallingCode = (code: CountryCode): string => {
     return countryCodeToCountryDataMap[code].callingCode;
+}
+
+export const getCountryByCallingAndAreaCode = (callingCode: string): CountryCode => {
+    return callingCodeToCountryCodeMap[callingCode];
 }
 
 export const getCountryFlag = (code: CountryCode): string => {
